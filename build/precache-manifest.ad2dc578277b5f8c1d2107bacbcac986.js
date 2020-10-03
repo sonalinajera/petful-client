996e8e34c62e1e4496454601145638d4266c1663
Sonali Najera <sonalitzul@gmail.com>
2020-10-02 17:57:26 -0700

    prepped to deploy

---
+++ build/precache-manifest.ad2dc578277b5f8c1d2107bacbcac986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4111a1a7777581b38305e4fab006a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11f68c74c303ff9cd546",
+    "url": "/static/css/main.7a5bf91a.chunk.css"
+  },
+  {
+    "revision": "dbf9d81f879609cda5c6",
+    "url": "/static/js/2.e7c4c3d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e7c4c3d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f68c74c303ff9cd546",
+    "url": "/static/js/main.9f65e7ba.chunk.js"
+  },
+  {
+    "revision": "10873a0725b3f95f5483",
+    "url": "/static/js/runtime-main.ee0db9a4.js"
+  }
+]);
