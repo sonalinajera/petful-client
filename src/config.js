5a9c7805998b1c521c1bafb942ca792702f8c3e4
Sonali Najera <sonalitzul@gmail.com>
2020-10-08 21:30:11 -0700

    implemented final user story

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
 export default {
-  // REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'https://petful-petbuddy.herokuapp.com'
-  REACT_APP_API_BASE: 'http://localhost:8080'
-
+  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'https://petful-petbuddy.herokuapp.com'
 };
--- src/routes/Adopt.js
+++ src/routes/Adopt.js
@@ -15,7 +15,8 @@ export class Adopt extends Component {
       pets: {},
       OK: false,
       fullName: '',
-      currentUser: ''
+      currentUser: '',
+      isHidden: true
     }
   }
 
@@ -40,39 +41,58 @@ export class Adopt extends Component {
 
   petCountdown = () => {
     var countdown = setInterval(() => {
-      if(this.state.people.length < 2) {
+      if (this.state.people.length < 2) {
         this.addToQueue();
-       return clearInterval(countdown);
+        this.setState({
+          isHidden: false
+        })
+        return clearInterval(countdown);
       }
-        fetch(`${config.REACT_APP_API_BASE}/people`, {
-          method: 'DELETE',
-          header: {
-            'content-type': 'application/json',
-          }
-        }).then(() => this.fetchData());
-        this.adopted();
-      }
+      fetch(`${config.REACT_APP_API_BASE}/people`, {
+        method: 'DELETE',
+        header: {
+          'content-type': 'application/json',
+        }
+      }).then(() => this.fetchData());
+      this.adopted();
+    }
       , 5000);
-    
-      // if(this.state.people.length < 2) {
-      //   return clearInterval(countdown)
-      // stopInterval()
-    // }
+
+
   }
 
-  addToQueue=() =>  {
+  addToQueue = () => {
     let randomPeople = [
       "Lisa NewCarr",
       'Karen From Finance',
       'Helen of Troy',
       'Jake from StateFarm',
       'Johnny Jon',
-      'Tracy Wells'
+      'Tracy Wells',
+      'Butter Balls',
+      'Tyson Chicken',
+      'Just Jan',
+      'Katya Zamo',
+      'Trixie Mattel',
+      '3 Kids in a Trenchcoat',
+      'Mustashe Yua Question',
+      'Pearl Krabs',
+      'Patrick',
+      'Terry da Terrible',
+      'Keebler Elf',
+      'Jon Snow',
+      'Preston Periwinkle',
+      'Shawn Stacey',
+      'Stacey\'s Mom'
     ];
 
-    let index = Math.floor(Math.random() * randomPeople.length); 
-    let person = randomPeople[index];
-    setInterval(() => { 
+    var addPeeps = setInterval(() => {
+      if (this.state.people.length > 4) {
+        return clearInterval(addPeeps);
+      }
+      let index = Math.floor(Math.random() * (randomPeople.length));
+      let person = randomPeople[index];
+
       fetch(`${config.REACT_APP_API_BASE}/people`, {
         method: 'POST',
         headers: {
@@ -80,24 +100,27 @@ export class Adopt extends Component {
         },
         body: JSON.stringify({ person })
       })
-      .then(res => res.json()).then(() => this.fetchData());
-    }, 5000) 
+        .then(res => res.json()).then(() => this.fetchData());
+    }, 5000);
   }
 
   fetchData = () => {
     fetch(`${config.REACT_APP_API_BASE}/pets`)
       .then(res => res.json())
-      .then(res => this.setState({ cats: [res.cat],
-        dogs: [res.dog], pets: res, OK: true }));
+      .then(res => this.setState({
+        cats: [res.cat],
+        dogs: [res.dog], pets: res, OK: true
+      }));
     fetch(`${config.REACT_APP_API_BASE}/people`)
       .then(res => res.json())
       .then(res => {
-      this.setState({ people: res.people })});
+        this.setState({ people: res.people })
+      });
   }
 
-  adopted = () => {
-    let counter = this.state.people.length;
-    if (counter === 0 ) {
+  adopted = (petId = null) => {
+    let counter = petId ? petId : this.state.people.length;
+    if (counter === 0) {
       return;
     }
     if (counter % 2 === 0) {
@@ -131,17 +154,33 @@ export class Adopt extends Component {
       },
       body: JSON.stringify({ person })
     })
-    .then(res => res.json())
-    .then(resJson => {
-       this.setState({
-         people: [...resJson.people]
-       })
+      .then(res => res.json())
+      .then(resJson => {
+        this.setState({
+          people: [...resJson.people]
+        })
       })
-    .then(() => this.fetchData());
+      .then(() => this.fetchData());
     this.petCountdown();
   }
 
+  handleAdoptClick = (event) => {
+    event.preventDefault();
+    fetch(`${config.REACT_APP_API_BASE}/people`, {
+      method: 'DELETE',
+      header: {
+        'content-type': 'application/json',
+      }
+    }).then(() => this.fetchData());
+    this.adopted(event.target.value);
+    alert('You adopted a pet!');
+    this.setState({
+      isHidden: true
+    })
+  }
+
   render() {
+    let hiddenClass = this.state.isHidden ? 'hiddenClass' : '';
     let cats = this.state.cats ? this.state.cats : [];
     let catCard = cats.map(cat => {
       return (
@@ -167,6 +206,7 @@ export class Adopt extends Component {
           <p>Age: {dog.age}</p>
           <p>Breed: {dog.breed}</p>
           <p>{dog.name}'s Story: {dog.story}</p>
+
         </div>
       )
     })
@@ -176,7 +216,7 @@ export class Adopt extends Component {
     let nextInLine = people.map((people, i) => {
       return (
         <div key={i}>
-        <li className='people-list'>{people}</li>
+          <li className='people-list'>{people}</li>
         </div>
       )
     })
@@ -186,12 +226,13 @@ export class Adopt extends Component {
         <div>
           <h2>Look who's up!</h2>
           <h2>Cats</h2>
-
+          <button className={`adoptCat-button ${hiddenClass}`} onClick={this.handleAdoptClick} value='2'> Adopt this cat</button>
           {catCard}
+
         </div>
         <h2>Dogs</h2>
+        <button className={`adoptDog-button ${hiddenClass}`} onClick={this.handleAdoptClick} value='1'> Adopt this dog</button>
         {dogCard}
-
         <section>
           <h2>Who's in line to adopt?</h2>
           <ol className='landing-content'>
@@ -202,7 +243,7 @@ export class Adopt extends Component {
           <h1>Get in Line</h1>
           <label htmlFor='full-name'>Enter Your Name</label>
           <input onChange={(event) => this.setState({ fullName: event.currentTarget.value })} type='text' id='full-name' />
-          <br/>
+          <br />
           <button>Join Queue</button>
         </form>
       </div >
